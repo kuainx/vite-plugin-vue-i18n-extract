4c2039b21fa1d7bc4e91ea24d5468edebba6c9dc
KUAI <ekuai@foxmail.com>
2026-04-15 00:04:09 +0800

    init

---
+++ oxlint.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from '@standard-config/oxlint'
+// import { defineConfig } from 'oxlint'
+
+export default defineConfig({
+  react: false,
+  rules: {
+    'no-plusplus': 'off',
+    'prefer-template': 'off',
+    'no-console': 'off',
+    'no-negated-condition': 'off',
+    'no-promise-executor-return': 'off',
+    'typescript/no-restricted-types': 'off',
+    'unicorn/prefer-add-event-listener': 'off',
+    'import/unambiguous': 'off',
+    'no-warning-comments': 'off',
+    radix: 'off',
+    curly: ['error', 'multi-line', 'consistent'],
+    'typescript/explicit-module-boundary-types': [
+      'error',
+      {
+        allowHigherOrderFunctions: true,
+        // allowOverloadFunctions: true,
+        // allowedNames: ['use*'],
+      },
+    ],
+    'typescript/consistent-type-definitions': ['error', 'interface'],
+  },
+})
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  pack: {
+    dts: {
+      tsgo: true,
+    },
+    exports: true,
+  },
+  fmt: {
+    $schema: './node_modules/oxfmt/configuration_schema.json',
+    singleQuote: true,
+    semi: false,
+    sortImports: {
+      groups: [
+        ['side_effect', 'side_effect_style'],
+        { newlinesBetween: true },
+        'external',
+        'named-external',
+        { newlinesBetween: true },
+        'import',
+        'named-import',
+        { newlinesBetween: true },
+        'type',
+      ],
+      newlinesBetween: false,
+    },
+    sortTailwindcss: {},
+  },
+});
